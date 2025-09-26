101f2cd2da89b7d0eb3340b72f3ca56fbfa89114
Arnauw <arnaud.rabel@gmail.com>
2025-09-26 11:41:10 +0200

    test link symfony react

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomepageController extends AbstractController
+{
+    #[Route('/', name: 'api_home', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        $testArray = [
+            ['id' => 1, 'name' => 'Alice'],
+            ['id' => 2, 'name' => 'Bob'],
+            ['id' => 3, 'name' => 'Charlie'],
+        ];
+
+        return $this->json($testArray);
+    }
+}
