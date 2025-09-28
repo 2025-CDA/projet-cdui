d66b9cde0238f5105ffa2f13d3e914ff1298d961
Arnauw <arnaud.rabel@gmail.com>
2025-09-28 12:12:56 +0200

    removing some useless files, update readme

--- src-react/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-export const About = () => {
-    return (
-        <>
-            <div>
-                <h2>About Page</h2>
-            </div>
-        </>
-    )
-}
--- src-react/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-export const Contact = () => {
-    return (
-        <>
-            <div>
-                <h2>Contact Page</h2>
-            </div>
-        </>
-    )
-}
