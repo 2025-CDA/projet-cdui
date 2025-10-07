019ba5132a3506f797862fd0433974f23cc537d0
maxc0434 <max.couillet@gmail.com>
2025-10-07 15:28:25 +0200

    bdd

--- src/Entity/Organization.php
+++ src/Entity/Organization.php
@@ -13,8 +13,38 @@ class Organization
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $siret = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $organization_name = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getSiret(): ?string
+    {
+        return $this->siret;
+    }
+
+    public function setSiret(string $siret): static
+    {
+        $this->siret = $siret;
+
+        return $this;
+    }
+
+    public function getOrganizationName(): ?string
+    {
+        return $this->organization_name;
+    }
+
+    public function setOrganizationName(?string $organization_name): static
+    {
+        $this->organization_name = $organization_name;
+
+        return $this;
     }
 }
