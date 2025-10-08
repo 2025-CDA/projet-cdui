e5e7759c9269f31b867f999f47bf0d392432f37a
Arnauw <arnaud.rabel@gmail.com>
2025-10-08 11:24:38 +0200

    added WeekDays and InfoFormStatus

---
+++ src/Enum/InfoFormStatus.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enum;
+
+enum InfoFormStatus: string
+{
+    case COMPLETED_INTERN_VALIDATION = 'completed_intern';
+
+    case COMPLETED_COMPANY_VALIDATION = 'completed_company';
+
+    case COMPLETED_ORGANIZATION_VALIDATION = 'completed_organization';
+
+    case FULLY_COMPLETED = 'fully_completed';
+
+    case REJECTED = 'rejected';
+}
---
+++ src/Enum/WeekDays.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enum;
+
+enum WeekDays: string
+{
+    case MONDAY = 'Lundi';
+    case TUESDAY = 'Mardi';
+    case WEDNESDAY = 'Mercredi';
+    case THURSDAY = 'Jeudi';
+    case FRIDAY = 'Vendredi';
+    case SATURDAY = 'Samedi';
+}
