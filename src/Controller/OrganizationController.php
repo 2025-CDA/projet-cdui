e9ab484df166281e837d3b02e8aa8ae7532d9d89
maxc0434 <max.couillet@gmail.com>
2025-10-14 16:11:27 +0200

    organization sessions add edit

---
+++ src/Controller/OrganizationController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\TrainingSession;
+use App\Repository\OrganizationRepository;
+use App\Repository\TrainingSessionRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class OrganizationController extends AbstractController
+{
+    #[Route('/organization/sessions', name: 'organization-sessions')]
+    public function showOrganizationSessions(TrainingSessionRepository $trainingSessionRepository): Response
+    {
+        $sessions = $trainingSessionRepository->findAll();
+
+        return $this->json($sessions);
+    }
+
+    #[Route('/organization/sessions/add', name: 'organization-sessions-add')]
+    public function addOrganizationSessions(Request $request, EntityManagerInterface $entityManager, ): Response
+    {
+        $sessions = new TrainingSession();
+
+        $form = $this-> createForm(TrainingSessionFormType::class, $sessions);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $entityManager->persist($sessions);
+            $entityManager->flush();
+        
+            return $this-> redirectToRoute('organization-sessions');
+        }
+        
+        return $this->json($sessions);
+    }
+
+        #[Route('/organization/sessions/{id}/edit', name: 'organization-sessions-edit')]
+    public function editOrganizationSessions(Request $request, EntityManagerInterface $entityManager, TrainingSession $trainingSession ): Response
+    {
+
+        $form = $this-> createForm(TrainingSessionFormType::class, $trainingSession);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $entityManager->flush();
+        
+            return $this-> redirectToRoute('organization-sessions');
+        }
+        
+        return $this->json($trainingSession);
+    }
+}
