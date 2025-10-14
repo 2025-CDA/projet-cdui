4e8637c0670b1caf09069820c76c3f8bba03ceae
maxc0434 <max.couillet@gmail.com>
2025-10-14 09:32:46 +0200

    edit config to authentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,21 +10,21 @@ use App\Entity\User;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/api/login_check', name: 'app_login', methods: ['POST'])]
-    public function login(#[CurrentUser] ?User $user): Response
-    {
-        if (null === $user) {
-            return $this->json([
-                'message' => 'Missing credentials or invalid login',
-            ], Response::HTTP_UNAUTHORIZED);
-        }
+    // #[Route(path: '/api/login_check', name: 'app_login', methods: ['POST'])]
+    // public function login(#[CurrentUser] ?User $user): Response
+    // {
+    //     if (null === $user) {
+    //         return $this->json([
+    //             'message' => 'Missing credentials or invalid login',
+    //         ], Response::HTTP_UNAUTHORIZED);
+    //     }
 
-        // Ici vous pouvez renvoyer des infos sur l'utilisateur ou un token
-        return $this->json([
-            'user' => $user->getUserIdentifier(),
-            // 'token' => ... (le token JWT est envoyé automatiquement si LexikJWT est configuré)
-        ]);
-    }
+    //     // Ici vous pouvez renvoyer des infos sur l'utilisateur ou un token
+    //     return $this->json([
+    //         'user' => $user->getUserIdentifier(),
+    //         // 'token' => ... (le token JWT est envoyé automatiquement si LexikJWT est configuré)
+    //     ]);
+    // }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
