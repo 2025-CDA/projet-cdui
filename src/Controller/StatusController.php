b18d232f9c5593d1cc002191626dacf937e23ede
Imad <karimimadbz@gmail.com>
2025-10-15 14:55:09 +0200

    controllerstatus

---
+++ src/Controller/StatusController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\InfoFormRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class StatusController extends AbstractController
+{
+    #[Route('/status/{id}', name: 'app_status_form')]
+    public function showStatusForm(int $id, InfoFormRepository $infoFormRepository): Response
+    {
+        $statusForm = $infoFormRepository->find($id);
+        $statusData = $statusForm -> getStatus();
+        return $this->json($statusData);
+
+        dd($statusData);
+    }
+
+}
