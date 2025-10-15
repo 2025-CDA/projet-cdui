f6a489fedf697eaee0f7a47f12e8ad1f10d8f6e4
Imad <karimimadbz@gmail.com>
2025-10-15 15:41:00 +0200

    status infoFormIntern

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\InfoFormIntern;
+use App\Repository\InfoFormInternRepository;
 use App\Repository\InfoFormRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +16,16 @@ final class StatusController extends AbstractController
     {
         $statusForm = $infoFormRepository->find($id);
         $statusData = $statusForm -> getStatus();
+        return $this->json($statusData);
+
+        dd($statusData);
+    }
+
+     #[Route('/status/intern/{id}', name: 'app_status_intern_form')]
+    public function showInternStatusForm(int $id, InfoFormInternRepository $infoFormInternRepository): Response
+    {
+        $statusInternForm = $infoFormInternRepository->find($id);
+        $statusData = $statusInternForm -> getStatus();
         return $this->json($statusData);
 
         dd($statusData);
