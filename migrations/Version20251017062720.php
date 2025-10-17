1a94276c4b0fba07afc2c14482d46b60b96d9bb9
Julen37 <julen.gouchault@gmail.com>
2025-10-17 08:27:56 +0200

    fix database migration

--- migrations/Version20251017062720.php
+++ migrations/Version20251017062720.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251017061335 extends AbstractMigration
+final class Version20251017062720 extends AbstractMigration
 {
     public function getDescription(): string
     {
