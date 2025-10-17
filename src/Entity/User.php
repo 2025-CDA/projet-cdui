43c3588c0149bd9159eeafcccd278b8b2c7b00f5
Arnauw <arnaud.rabel@gmail.com>
2025-10-17 07:58:52 +0200

    fix UserRole important in User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -20,7 +20,6 @@ use App\Controller\User\CreateUserController;
 use App\Controller\User\UpdateUserController;
 use App\Enum\UserRole;
 use App\Repository\UserRepository;
-use App\Enum\UserRole;
 
 //use App\State\UserStateProcessor;
 use Doctrine\ORM\Mapping as ORM;
